8080bf3ae02fae52284ed9e9e5caf35997651122
Haja Asmath <asmath1997@gmail.com>
2022-07-23 23:24:46 +0530

    modified stocks prob, added spiral matrix and find sum

--- Week-6/maxSumArray/stocks.js
+++ Week-6/maxSumArray/stocks.js
@@ -1,29 +1,17 @@
 function sellStock(arr) {
-    let {min, index} = findSmallest(arr);
-    console.log(min);
     let maxProfit = 0;
-    if(index === arr.length-1) {
-        return maxProfit;
-    }
-    for(let i = index+1; i<arr.length;i++) {
-        if((arr[i] - min) > maxProfit) {
-            maxProfit = arr[i] - min;
+    let currentMin = Infinity;
+
+    for(let i = 0;i<arr.length;i++) {
+        if(arr[i] < currentMin) {
+            currentMin = arr[i];
         }
+        //currentMin = Math.min(arr[i], currentMin);
+        maxProfit = Math.max(maxProfit, arr[i] - currentMin);
+
     }
 
     return maxProfit;
 }
 
-function findSmallest(arr) {
-    let min = Infinity;
-    let index = 0;
-    for(let i = 0;i<arr.length;i++) {
-        if(arr[i] < min) {
-            min = arr[i];
-            index = i;
-        }
-    }
-    return {min : min, index: index};
-}
-
-console.log(sellStock([7,6,4,3,1]));
+console.log(sellStock([7,1,5,3,6,4]));
