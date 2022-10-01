46656e8f4beed2ceec0757e4019b25a1fa087d93
Haja Asmath <asmath1997@gmail.com>
2022-10-01 23:13:35 +0530

    week13 refactor

--- Week-13/WeatherServer.js/controller.js
+++ Week-13/WeatherServer.js/controller.js
@@ -1,26 +1,41 @@
 const service = require('./service');
 const url = require('url');
 
-const handleRequest = async (req, res) => {
+const handleRequest = (req, res) => {
     const path = url.parse(req.url).pathname;
-    if(path === '/') {
-        res.end('Weather Data Api')
-    } else if(path === '/getCity') {
-        const {cityName} = url.parse(req.url, true).query;
-        const result = await service.getWeatherDataByCity(cityName);
-        res.end(JSON.stringify(result));
-    } else if(path ==='/cities'){
-        let {cities} = url.parse(req.url, true).query;
-        cities = cities.split(',');
-        const result = await service.getWeatherDataByCities(cities);
-        res.end(JSON.stringify(result));
-    } else if(path === '/getForcast') {
-        let {days} = url.parse(req.url, true).query;
-        const result = await service.getForcastForXdays(days);
-        res.end(JSON.stringify(result));
+    if(controller[path]) {
+        controller[path](req, res);
     } else {
-        res.end('Invalid Request');
+        res.end("Invalid Request");
     }
 }
 
-module.exports = {handleRequest};
+const controller = {
+    "/getCity" : async (req, res) => {
+        const {
+            cityName
+        } = url.parse(req.url, true).query;
+        const result = await service.getWeatherDataByCity(cityName);
+        res.end(JSON.stringify(result));
+    }, 
+    "/cities" : async (req, res) => {
+        let {
+            cities
+        } = url.parse(req.url, true).query;
+        cities = cities.split(',');
+        const result = await service.getWeatherDataByCities(cities);
+        console.log(JSON.stringify(result));
+        res.end(JSON.stringify(result));
+    },
+    "/getForcast" : async (req, res) => {
+        let {
+            days
+        } = url.parse(req.url, true).query;
+        const result = await service.getForcastForXdays(days);
+        res.end(JSON.stringify(result));
+    }
+}
+
+module.exports = {
+    handleRequest
+};
--- Week-13/WeatherServer.js/service.js
+++ Week-13/WeatherServer.js/service.js
@@ -1,32 +1,41 @@
 const axios = require('axios');
-
-const GEO_CODE_URL = 'https://api.openweathermap.org/data/2.5/weather';
-const WEATHER_DATA_URL = 'https://api.openweathermap.org/data/2.5/onecall';
-const API_KEY = '3bea71f7f942028431554879c47b3585';
+const path = require('path');
+require("dotenv").config();
 const EXCLUDE = 'minutely';
 const CURRENT_LAT = 13.0878;
 const CURRENT_LON = 80.2785;
 
 const getWeatherDataByCity = async (cityName) => {
-    const result = await axios.get(`${GEO_CODE_URL}?q=${cityName}&appid=${API_KEY}`);
+    const result = await axios.get(`${process.env.GEO_CODE_URL}?q=${cityName}&appid=${process.env.API_KEY}`);
     return result.data;
 }
 
 const getWeatherDataByCities = async (cities) => {
-    const result = [];
+    let result = [];
+    let promiseArray = [];
     for(let city of cities) {
-        console.log(city);
-        const res = await axios.get(`${GEO_CODE_URL}?q=${city}&appid=${API_KEY}`);
-        result.push(res.data);
+        const promise = new Promise((resolve, reject) => {
+            resolve(axios.get(`${process.env.GEO_CODE_URL}?q=${city}&appid=${process.env.API_KEY}`));
+        }).then(res => {
+            if(res.status !== 200){
+                throw new Error("Request Failed");
+            }
+            return res.data;
+        });
+        promiseArray.push(promise);
     }
+    result = await Promise.all(promiseArray).then(res => {
+        return res;
+    }).catch(err => {
+        return err.message;
+    });
     return result;
 }
 
 const getForcastForXdays = async (days) => {
     if(days <= 8) {
-        const res = await axios.get(`${WEATHER_DATA_URL}?lat=${CURRENT_LAT}&lon=${CURRENT_LON}&exclude=${EXCLUDE}&appid=${API_KEY}`);
+        const res = await axios.get(`${process.env.WEATHER_DATA_URL}?lat=${CURRENT_LAT}&lon=${CURRENT_LON}&exclude=${EXCLUDE}&appid=${process.env.API_KEY}`);
         const {daily} = res.data;
-        console.log(daily);
         return daily.slice(0, days);
     } else {
         return 'Only 8 days forcast available';
