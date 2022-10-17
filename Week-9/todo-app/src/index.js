60fd21808fb44ea25b7e51b1b58ff9b781f3b43a
Haja Asmath <asmath1997@gmail.com>
2022-10-18 02:26:33 +0530

    week16

--- Week-9/todo-app/src/index.js
+++ Week-9/todo-app/src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import Header from './header/header';
 import Content from './contentComponent/content'
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://eb19d097bcbe4c7d90132200b06cf176@o4504000839352320.ingest.sentry.io/4504000841187329",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
